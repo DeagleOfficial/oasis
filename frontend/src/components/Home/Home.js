11177d6a8fde02f58c910cce68c8b8e28eced706
DeagleOfficial <supratikofficial1@gmail.com>
2022-07-08 21:04:37 +0530

    Added files

---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+import axios from "axios";
+
+import DeleteIcon from "@material-ui/icons/Delete";
+
+const Home = () => {
+    const [noteList, setNotes] = useState([]);
+
+    const callFn = () => {
+        const token = localStorage.getItem("token");
+
+        axios
+            .get(`${process.env.REACT_APP_OASIS_BACKEND}/notes`, {
+                headers: { Authorization: `Bearer ${token}` },
+            })
+            .then((res) => {
+                console.log(res);
+                setNotes(res.data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    };
+
+    useEffect(() => {
+        callFn();
+    }, []);
+
+    useEffect(() => {
+        callFn();
+    }, [setNotes]);
+
+    return (
+        <div className="Home">
+            <h1 className="HomeNotes">Notes</h1>
+
+            {!noteList ||
+                (noteList.length == 0 && (
+                    <h2 className="NoNotesFound">No Notes Found</h2>
+                ))}
+            <div className="NoteList container">
+                {noteList && (
+                    <div>
+                        {" "}
+                        {noteList.map((note) => (
+                            <div className="Note">
+                                <div className="NoteContent">
+                                    {note.content}
+                                </div>
+                                <Link to={`/deletetask/${note._id}`}>
+                                    <span className="DelIcon">
+                                        <DeleteIcon />
+                                    </span>
+                                </Link>
+                            </div>
+                        ))}{" "}
+                    </div>
+                )}
+            </div>
+            <div>
+            <Link id = "addlink" to="/create">
+                <button className="AddBtn btn btn-lg">+</button>
+            </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
